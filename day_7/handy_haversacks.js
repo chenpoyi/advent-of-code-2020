43a2fa70d977300660406fc80b7865c3ec2a2435
Paul Chen <paulchen647@gmail.com>
2020-12-12 17:23:41 +0800

    Finish day 7 part 2

--- day_7/handy_haversacks.js
+++ day_7/handy_haversacks.js
@@ -3,11 +3,9 @@ const file = fs.readFileSync("./handy_haversacks.txt").toString("utf-8");
 const rows = file.split("\n");
 
 const part_1 = function () {
-  //console.log(rows)
   let colors = [];
   const newArray = process(rows);
 
-  //console.log(newArray);
   colors.push(...search(newArray, "shiny gold"));
   const uniqueColors = [];
   colors.forEach((color)=>{
@@ -29,15 +27,11 @@ const process = function (array) {
     obj.children = children;
     return obj;
   });
-  //return obj;
 };
 
 const search = function (array, color) {
   let colors = [];
-  // console.log('array: ', array);
-  // console.log('color: ', color)
   array.forEach((obj) => {
-    //console.log(obj.children);
     if (obj.children.find((child) => child === color)) {
       colors.push(obj.parent);
     }
@@ -50,4 +44,49 @@ const search = function (array, color) {
   colors = colors.concat(newColors)
   return colors;
 };
+
+const part_2 = function(){
+    const bagsMap = process2(rows);
+    return countTotal('shiny gold', bagsMap);
+};
+
+const process2 = function(array){
+  const map = {};
+  array.forEach((row)=>{
+    // console.log(row)
+
+    const split = row.split(' bags contain ')
+    const parent = split[0];
+    const obj = {};
+    split[1].split(", ").forEach((child) => {
+      const number = child.substr(0,child.indexOf(' '));
+      const color = child.substr(child.indexOf(' ')+1).substr(0, child.substr(child.indexOf(' ')+1).indexOf(' bag'));
+      // const obj = {};
+      obj[color] = number;
+      
+    });
+    map[parent] = obj;
+    
+  })
+  return map;
+}
+
+const countTotal = function(color, map){
+  let count = 0;
+  
+  for(let child in map[color]){
+
+    if(Object.keys(map[child])[0] === 'other'){
+      count += Number(map[color][child]);
+    } else {
+      count+= Number(map[color][child]) + Number(map[color][child]) * countTotal(child, map)
+    }
+   
+  }
+  return count;
+
+}
+
 console.log(part_1());
+console.log(part_2());
+
