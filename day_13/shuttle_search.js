756fd020d94bc517817ae2eaca028cfb735828e8
Paul Chen <paulchen647@gmail.com>
2020-12-15 22:50:52 +0800

    Attempted day 13 part 2

--- day_13/shuttle_search.js
+++ day_13/shuttle_search.js
@@ -11,4 +11,42 @@ const part_1 = function(){
   return time[0][0] * time[0][1];
 };
 
-console.log(part_1());
+const part_2 = function(){
+  const schedule = rows[1].split(',').map((element, index) => element === 'x' ? ['x'] : [Number(element), index]).filter(element => element[0] != 'x');
+  let departure = 152682;
+  console.log(schedule);
+  while(true){
+    if(check(schedule, departure)){
+      return departure * schedule[0][0];
+    }
+    console.log(departure * schedule[0][0]);
+    departure++;
+  }
+}
+
+// const part_2 = function(){
+//   const schedule = rows[1].split(',').map(element => element === 'x' ? 'x' : Number(element));
+//   let departure = 152682;
+//   console.log(schedule);
+//   schedule.forEach(([busNumber, index]) => {
+//     if(busNumber != 'x'){
+        
+//     }
+//   })
+// }
+const check = function(schedule, departure){
+  for(let i = 1; i < schedule.length; i++){
+    const first = schedule[0][0];
+    // console.log('first: ', first);
+    // console.log('departure: ', departure);
+    // console.log('i: ', i);
+    // console.log('timestamp: ', (first * departure+i)%schedule[i])
+    if(!((first * departure+schedule[i][1])%schedule[i][0] === 0)){
+      // console.log('hi');
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(part_1());
+console.log(part_2());
